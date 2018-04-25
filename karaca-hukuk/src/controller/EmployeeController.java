d41405f1025ee80b6a3c21713cdecc3a5a14255e
erkanzileli <erkanzileli@gmail.com>
2018-04-25 12:22:27 +0300

    employeeDetails-filter

--- karaca-hukuk/src/controller/EmployeeController.java
+++ karaca-hukuk/src/controller/EmployeeController.java
@@ -2,12 +2,16 @@ package controller;
 
 import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableView;
 import dao.EmployeeDAO;
 import daoImpl.EmployeeDAOImpl;
+import entity.Employee;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
@@ -19,25 +23,41 @@ public class EmployeeController implements Initializable {
     @FXML
     private StackPane root;
 
-    public static JFXDialog createEmployeeDialog;
+    @FXML
+    private JFXTreeTableView<?> tableOfEmployees;
 
-    private EmployeeDAO employeeDAO;
+    @FXML
+    private TreeTableColumn<?, ?> columnName;
+
+    @FXML
+    private TreeTableColumn<?, ?> columnSurname;
+
+    @FXML
+    private TreeTableColumn<?, ?> columnTC;
+
+    @FXML
+    private TreeTableColumn<?, ?> columnType;
+
+    @FXML
+    private TreeTableColumn<?, ?> columnEmail;
+
+    @FXML
+    private TreeTableColumn<?, ?> columnMobilePhone;
+
+    @FXML
+    private JFXTextField textSearch;
+
+    static JFXDialog createEmployeeDialog;
+
+    private static JFXDialog employeeDetailsDialog;
 
     @FXML
     private void showCreateEmployee() {
-        Parent createEmployee = null;
-        try {
-            createEmployee = FXMLLoader.load(getClass().getResource("/fxml/createEmployee.fxml"));
-        } catch (IOException ignored) {
-            System.out.println(ignored.getMessage());
-        }
-        JFXDialogLayout jfxDialogLayout = new JFXDialogLayout();
-        jfxDialogLayout.setBody(createEmployee);
-        JFXDialog dialog = new JFXDialog(root, jfxDialogLayout, JFXDialog.DialogTransition.CENTER);
-        createEmployeeDialog = dialog;
-        dialog.setOverlayClose(false);
-        dialog.show();
+        createEmployeeDialog = createDialog("createEmployee", JFXDialog.DialogTransition.TOP);
+        createEmployeeDialog.setOverlayClose(false);
+        createEmployeeDialog.show();
     }
+
 
     @FXML
     private void search() {
@@ -46,7 +66,33 @@ public class EmployeeController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        employeeDAO = new EmployeeDAOImpl();
+    }
+
+    //todo:tablodaki elemanlardan birine tıklandığında
+    private void showEmployeeDetails() {
+        employeeDetailsDialog = createDialog("employeeDetails", JFXDialog.DialogTransition.CENTER);
+        employeeDetailsDialog.setOverlayClose(false);
+        employeeDetailsDialog.show();
+    }
+
+    private JFXDialog createDialog(String fxmlName, JFXDialog.DialogTransition dialogTransition) {
+        Parent employeeDetails = null;
+        try {
+            employeeDetails = FXMLLoader.load(getClass().getResource("/fxml/" + fxmlName + ".fxml"));
+        } catch (IOException ignored) {
+            System.out.println(ignored.getMessage());
+        }
+        JFXDialogLayout jfxDialogLayout = new JFXDialogLayout();
+        jfxDialogLayout.setBody(employeeDetails);
+        return new JFXDialog(root, jfxDialogLayout, dialogTransition);
+    }
+
+    static JFXDialog getCreateEmployeeDialog() {
+        return createEmployeeDialog;
+    }
+
+    static JFXDialog getEmployeeDetailsDialog() {
+        return employeeDetailsDialog;
     }
 
 }
